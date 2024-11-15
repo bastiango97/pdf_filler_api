17443347feab131407d468da8f9b8e1045b95e63
bastiango97 <sebas.gono@gmail.com>
2024-11-15 11:12:31 -0600

    Remove ignored files from tracking

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 const authenticateToken = require('./middleware/authenticateToken');
